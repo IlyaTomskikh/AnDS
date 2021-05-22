16b2e2bb2bdffd7406ef671971860982eef9335c
Ilya Tomskikh <69895429+TeaEye@users.noreply.github.com>
2021-05-22 16:04:35 +0700

    Update BigBum.cpp

--- BigBum.cpp
+++ BigBum.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <time.h>
 #include <string.h>
+#define BASE_SIZE (sizeof(BASE)*8)
 using namespace std;
 typedef unsigned short BASE;
 typedef unsigned int DBASE;
-#define BASE_SIZE (sizeof(BASE)*8)
+
 
 class BigNum
 {
@@ -16,14 +17,9 @@ public:
 	BigNum(int l = 1, bool flag = true): len(l), maxLen(l)
 	{
 		digits = new BASE[maxLen];
-		if (flag)
-		{
-			//len = 1;
-			for(int i = 0; i < maxLen; ++i) digits[i] = 0;
-		}
+		if (flag) for(int i = 0; i < maxLen; ++i) digits[i] = 0;
 		else
 		{
-			srand(time(0));
 			for(int i = 0; i < maxLen; ++i) digits[i] = rand();
 			for(int i = maxLen; i--;) if (digits[i] == 0) --len;
 		}
@@ -40,19 +36,27 @@ public:
 		delete[] digits;
 		digits = NULL;
 	}
-	BigNum & operator= (BigNum &that)
+	BigNum & operator= (const BigNum &that)
 	{
-		if (this == &that) return *this;
-		len = that.len;
-		maxLen = that.maxLen;
-		delete[] digits;
-		digits = new BASE[maxLen];
-		for (int i = 0; i < maxLen; ++i) digits[i] = that.digits[i];
+		if (this != &that)
+		{
+			len = that.len;
+			maxLen = that.maxLen;
+			delete[] digits;
+			digits = new BASE[maxLen];
+			for (int i = 0; i < maxLen; ++i) digits[i] = that.digits[i];
+		}
 		return *this;
 	}
+	
+	BASE & operator[](int ix)
+	{
+		return digits[ix];
+	}
+
 	friend istream & operator>> (istream &in, BigNum &that)
 	{
-		std::string str;
+		string str;
 		in >> str;
 		if (str.length() > that.maxLen) return in;
 		delete[] that.digits;
@@ -64,8 +68,15 @@ public:
 		for (int i = str.length(); i--;)
 		{
 			if (str[i] <= '9' && str[i] >= '0') tmp = str[i] - '0';
-			if (str[i] <= 'f' && str[i] >= 'a') tmp = str[i] - 'a' + 10;
-			if (str[i] <= 'F' && str[i] >= 'A') tmp = str[i] - 'A' + 10;
+			else
+			{
+				if (str[i] <= 'f' && str[i] >= 'a') tmp = str[i] - 'a' + 10;
+				else
+				{
+					if (str[i] <= 'F' && str[i] >= 'A') tmp = str[i] - 'A' + 10;
+					else break;
+				}
+			}
 			that.digits[j] |= tmp << (k*4);
 			++k;
 			if (k >= BASE_SIZE / 4)
@@ -75,51 +86,52 @@ public:
 				++j;
 			}
 		}
-		for(int i = that.maxLen; i--;) if (that.digits[i] == 0) --(that.len);
+		that.lenNorm();
 		return in;
 	}
 	friend ostream & operator<< (ostream &out, BigNum &that)
 	{
 		cout.width(BASE_SIZE / 4);
 		cout.fill('0');
-		if (that.digits != NULL) for (int i = that.len; i--;) out << hex << that.digits[i];
+		if (that.digits != NULL) for (int i = that.len; i--;) out << hex << int(that.digits[i]);
 		else out << ' ';
 		cout << dec;
 		return out;
 	}
-	bool operator== (BigNum &that)
+	
+	bool operator== (const BigNum &that)
 	{
 		if (len != that.len) return false;
 		for (int i = 0; i < len; ++i) if (digits[i] != that.digits[i]) return false;
 		return true;
 	}
-	bool operator!= (BigNum &that)
+	bool operator!= (const BigNum &that)
 	{
 		return !(*this == that);
 	}
-	bool operator< (BigNum &that)
+	bool operator< (const BigNum &that)
 	{
 		if (this == &that) return false;
 		if (len != that.len) return len < that.len;
-		for (int i = 0; i < len; ++i) if (digits[i] != that.digits[i]) return digits[i] < that.digits[i];
+		for (int i = len; i--;) if (digits[i] != that.digits[i]) return digits[i] < that.digits[i];
 		return false;
 	}
-	bool operator> (BigNum &that)
+	bool operator> (const BigNum &that)
 	{
 		if (this == &that) return false;
-		if (len != that.len) return len < that.len;
-		for (int i = 0; i < len; ++i) if (digits[i] != that.digits[i]) return digits[i] > that.digits[i];
+		if (len != that.len) return len > that.len;
+		for (int i = len; i--;) if (digits[i] != that.digits[i]) return digits[i] > that.digits[i];
 		return false;
 	}
-	bool operator<= (BigNum &that)
+	bool operator<= (const BigNum &that)
 	{
 		return !(*this > that);
 	}
-	bool operator>= (BigNum &that)
+	bool operator>= (const BigNum &that)
 	{
 		return !(*this < that);
 	}
-	BigNum operator+ (BigNum &that)
+	BigNum operator+ (const BigNum &that)
 	{
 		BigNum sum(max(maxLen, that.maxLen) + 1);
 		int nm = min(len, that.len), j = 0, k = 0;
@@ -149,11 +161,31 @@ public:
 		sum.lenNorm();
 		return sum;
 	}
-	BigNum operator+= (BigNum &that)
+	BigNum operator+= (const BigNum &that)
 	{
-		return (*this + that);
+		*this = *this + that;
+		return *this;
 	}
-	BigNum operator- (BigNum &that)
+	BigNum operator+ (BASE num)
+	{
+		int k = num, i = 0;
+		BigNum sum(maxLen + 1);
+		while (i < len)
+		{
+			DBASE tmp = digits[i] + k;
+			k = (BASE) (tmp >> BASE_SIZE);
+			sum.digits[i] = tmp;
+			++i;
+		}
+		sum.digits[i] = k;
+		return sum;
+	}
+	BigNum operator+= (BASE num)
+	{
+		*this = *this + num;
+		return *this;
+	}
+	BigNum operator- (const BigNum &that)
 	{
 		BigNum sub(max(len, that.len), true);
 		if (*this >= that)
@@ -166,11 +198,12 @@ public:
 				tmp -= (that.digits[j] + k);
 				sub.digits[j] = (BASE) tmp;
 				k = !(tmp >> BASE_SIZE);
+				// ! is safer then ~
 				++j;
 			}
 			while (j < len)
 			{
-				tmp = (1 << BASE_SIZE) + that.digits[j];
+				tmp = (1 << BASE_SIZE) + digits[j];
 				tmp -= k;
 				sub.digits[j] = (BASE) tmp;
 				k = !(tmp >> BASE_SIZE);
@@ -178,11 +211,17 @@ public:
 			}
 			sub.lenNorm();
 		}
+		else
+		{
+			cerr << "right operand must be less or equal" << endl;
+			exit(-1);
+		}
 		return sub;
 	}
-	BigNum operator-= (BigNum &that)
+	BigNum operator-= (const BigNum &that)
 	{
-		return (*this - that);
+		*this = *this - that;
+		return *this;
 	}
 	BigNum operator* (BASE num)
 	{
@@ -198,30 +237,35 @@ public:
 		mul1.lenNorm();
 		return mul1;
 	}
+	BigNum operator*= (BASE num)
+	{
+		*this = *this * num;
+		return *this;
+	}
 	BigNum operator* (BigNum &that)
 	{
-		BigNum mul2 (len + that.len - 1, true);
-		for (int j = 0; j < len; ++j)
+		BigNum mul(len + that.len, true);
+		DBASE tmp, k;
+		for (int i = 0; i < len; ++i)
 		{
-			if (that.digits[j] != 0)
+			k ^= k;
+			for (int j = 0; j < that.len; ++j)
 			{
-				int i = 0, k = 0;
-				DBASE tmp;
-				for (; i < that.len; ++i, k = tmp >> BASE_SIZE)
-				{
-					tmp = digits[i] * (that.digits[j]) + mul2.digits[i + j] + k;
-					mul2.digits[i + j] = (BASE) tmp;
-				}
+				tmp = (DBASE) that[j] * digits[i] + mul[i + j] + k;
+				mul[i + j] = tmp;
+				k = tmp >> BASE_SIZE;
 			}
+			mul[that.len + i] += k;
 		}
-		mul2.lenNorm();
-		return mul2;
+		mul.lenNorm();
+		return mul;
 	}
 	BigNum operator*= (BigNum &that)
 	{
-		return ((*this) * that);
+		*this = (*this) * (that);
+		return *this;
 	}
-	BigNum operator/ (BASE num)
+	BigNum operator/ (const BASE num)
 	{
 		if (num <= 0) return 0;
 		BigNum div1(len, true);
@@ -237,7 +281,7 @@ public:
 		}while (j < len);
 		return div1;
 	}
-	BASE operator% (BASE num)
+	BASE operator% (const BASE num)
 	{
 		if (num <= 0) return 0;
 		BASE module1;
@@ -253,6 +297,7 @@ public:
 		}while (j < len);
 		return module1;
 	}
+	/*
 	BigNum operator/ (BigNum &that)
 	{
 		BigNum zer(that.len, true), one(that.len, true);
@@ -261,10 +306,30 @@ public:
 		if (that <= zer || *this < that) return zer;
 		BigNum div2(len, true);
 		BigNum r(that.len, true);
-
+		DBASE d = (that.digits[that.len - 1] + 1) >> BASE_SIZE;
+		*this = *this / 10 * d;
+		that = that * d;
+		int m = len - that.len;
+		BASE _r, _q;
+		do
+		{
+			_q = (digits[m + that.len] << BASE_SIZE + digits[m + that.len - 1]) / that.digits[that.len - 1];
+			_r = (digits[m + that.len] << BASE_SIZE + digits[m + that.len - 1]) % that.digits[that.len - 1];
+			do
+			{
+				if (_q << BASE_SIZE == _q * _q || _q * that.digits[that.len - 2] > _r << BASE_SIZE + digits[m + that.len - 2])
+				{
+					--_q;
+					_r += that.digits[that.len - 1];
+				}
+			} while(_r << BASE_SIZE == _r * _r);
+			//тут какое-то волшебство с вычетанием из определенной части числа
+		} //while ();
+		
 		div2.lenNorm();
 		return div2;
 	}
+	
 	BigNum operator% (BigNum &that)
 	{
 		BigNum zer(that.len, true);
@@ -276,43 +341,59 @@ public:
 		if (module2 >= that) return zer;
 		return module2;
 	}
-
+	*/
+/*
 	friend istream & operator>> (istream &in, BigNum &that)
 	{
-		std::string s;
+		string s;
 		in >> s;
-		if (s.length() > that.maxLen) return in;
+		DBASE tmp;
+		BASE k;
+		that.maxLen = (s.length() - 1) / (BASE_SIZE / 4) + 2;
+		that.len = 1;
 		delete[] that.digits;
-		that.len = (s.length() - 1) / (BASE_SIZE / 4) + 1;
-		that.digits = new BASE[that.len];
-		int j = 0;
-		//создаем большое число, которое будет состоять из одной ячейки типа BASE
-		BigNum t(1, true);
-		for (int i = that.len; i--;) that.digits[i] = 0;
-		do
+		that.digits = new BASE[that.maxLen];
+		for (int i = that.maxLen - 1; i--;) that.digits[i] = 0;
+		for (int i = 0; i < that.maxLen; ++i)
 		{
-			t.digits[0] = s[j] - '0';
-			that = that * 10 + t;
-			++j;
-		} while (j < s.length());
+			if (s[i] >= '0' && s[i] <= '9')
+			{
+				that *= 10;
+				that += (s[i] - '0');
+			}
+			else break;
+		}
+		that.lenNorm();
 		return in;
 	}
 	friend ostream & operator<< (ostream &out, BigNum &that)
 	{
-		//cout.width(BASE_SIZE / 4);
-		//cout.fill('0');
-		if (that.digits != NULL) for (int i = that.len; i--;) out << that.digits[i];
-		else out << ' ';
+		string s = "";
+		for (int i = that.len - 1; i--;) s.push_back(char(that % ((BASE) 10) + 48));
+		out << s;
 		return out;
 	}
-
+*/
 	void lenNorm()
 	{
-		for(int i = maxLen; i--;) if (digits[i] == 0) --len;
+		len = maxLen;
+		for(int i = maxLen - 1; i > 0; --i) if (digits[i] == 0) --len;
 	}
 };
 
 int main()
 {
-	return 0;
+	srand(time(0));
+	BigNum a(4, false), b(4, false), c, d(4, false);
+	cout << "a = " << a << endl << "b = " << b << endl << "d = " << d << endl;
+	c = a + b;
+	cout << "a + b = " << c << endl;
+	c -= a;
+	cout << "a + b - a = " << c << endl;
+	c = a * b;
+	cout << "a * b = " << c << endl;
+	c *= d;
+	cout << "(a * b) * d = " << c << endl;
+	//calculatori.ru
+	return 1;
 }
