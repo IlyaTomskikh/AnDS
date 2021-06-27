7cbf13a4c530775ebdea09013d37be131b47331c
Ilya Tomskikh <69895429+TeaEye@users.noreply.github.com>
2021-06-27 14:42:58 +0700

    Update BigBum.cpp

--- BigBum.cpp
+++ BigBum.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <time.h>
 #include <string.h>
+#include <algorithm>
 #include <limits>
 #define BASE_SIZE (sizeof(BASE)*8)
+#define MAX_BASE_VALUE ((DBASE) 1 << BASE_SIZE)
 using namespace std;
 typedef unsigned short BASE;
 typedef unsigned int DBASE;
-BASE MAX_BASE_VALUE = ~0;
 
 
 class BigNum
@@ -16,86 +17,104 @@ private:
 	int maxLen;
 	BASE *digits;
 public:
+	
+//	DEC CIN/COUT
+	friend istream & operator>> (istream &in, BigNum &that)
+	{
+		string s;
+		in >> s;
+		DBASE tmp = 0;
+		BASE k = 0;
+		delete[] that.digits;
+		that.len = 1;
+		that.maxLen = (s.length() - 1) / (BASE_SIZE / 4) + 1;
+		that.digits = new BASE[that.maxLen];
+		for (int i = 0; i < that.maxLen; ++i) that.digits[i] = 0;
+		for (int i = 0; i < s.length(); ++i)
+		{
+			char _ch;
+			if (s[i] >= '0' && s[i] <= '9')
+			{
+				_ch = s[i] - '0';
+				that *= 10;
+				that +=_ch;
+			}
+		}
+		that.lenNorm();
+		return in;
+	}
+	friend ostream & operator<< (ostream &out, BigNum &that)
+	{
+		if (!(that.digits[0] == 0 && that.len == 1))
+		{
+			string s = "";
+			BigNum tmp(that);
+			while (tmp.digits[0] != 0 || tmp.len > 1)
+			{
+				char _ch = tmp % 10 + '0';
+				s.push_back(_ch);
+				tmp /= 10;
+			}
+			reverse(s.begin(), s.end());
+			out << s;
+		}
+		else out << 0;
+		return out;
+	}
 
-	int weirdSub(const BigNum &that, int ix)
-	{
-		DBASE tmp;
-		int k = 0;
-		for (int i = 0; i < that.len; ++i)
-		{
-			tmp = digits[i + ix] + MAX_BASE_VALUE - that.digits[i] - k;
-			digits[ix + i] = (BASE) tmp;
-			k = !(tmp >> BASE_SIZE);
-		}
-		for (int i = that.len; k && i <= len; ++i, k = !(tmp >> BASE_SIZE))
-		{
-			tmp = digits[ix + i] + MAX_BASE_VALUE - k;
-			digits[i + ix] = (BASE) tmp;
-		}
-		lenNorm();
-		return k;
-	}
-	void weirdAdd(BigNum &that, int ix)
-	{
-		int k = 0;
-		for (int i = 0; i < that.len - 2; ++i)
-		{
-			DBASE tmp = digits[ix + i] + that.digits[i] + k;
-			k = tmp >> BASE_SIZE;
-			digits[i + ix] = tmp;
-		}
-		lenNorm();
-	}
 
 	BigNum(int l = 1, bool flag = true): len(l), maxLen(l)
 	{
-		digits = new BASE[maxLen];
-		if (flag) for(int i = 0; i < maxLen; ++i) digits[i] = 0;
+		digits = new BASE[len];
+		if (flag) for(int i = 0; i < len; ++i) digits[i] = 0;
 		else
 		{
-			for(int i = 0; i < maxLen; ++i) digits[i] = rand();
-			for(int i = maxLen; i--;) if (digits[i] == 0) --len;
+			for(int i = 0; i < len; ++i)
+			{
+				digits[i] = rand();
+				if (BASE_SIZE >= sizeof(int) * 8) 
+				{
+					digits[i] = digits[i] << BASE_SIZE / 2;
+					digits[i] += rand();
+				}
+			}
+			lenNorm();
 		}
 	}
 	BigNum(const BigNum &that): len(that.len), maxLen(that.maxLen)
 	{
 		digits = new BASE[maxLen];
 		for (int i = 0; i < maxLen; ++i) digits[i] = that.digits[i];
+		lenNorm();
 	}
 	~BigNum()
 	{
-		len = 0;
-		maxLen = 0;
+		len ^= len;
+		maxLen ^= maxLen;
 		delete[] digits;
-		digits = NULL;
+		digits = nullptr;
 	}
 	BigNum & operator= (const BigNum &that)
 	{
 		if (this != &that)
-		{
+		{	
 			len = that.len;
 			maxLen = that.maxLen;
 			delete[] digits;
 			digits = new BASE[maxLen];
-			for (int i = 0; i < maxLen; ++i) digits[i] = that.digits[i];
+			for (int i = 0; i < maxLen; i++) digits[i] = that.digits[i];
 		}
 		return *this;
 	}
-	BigNum & operator= (const BASE that)
-	{
-		delete[] digits;
-		len = 1;
-		digits = new BASE[1];
-		digits[0] = that;
-		return *this;
-	}
+
 	
 	BASE & operator[](int ix)
 	{
 		return digits[ix];
 	}
 
-
+//	HEX CIN/COUT
+/*
 	friend istream & operator>> (istream &in, BigNum &that)
 	{
 		string str;
@@ -133,19 +152,35 @@ public:
 	}
 	friend ostream & operator<< (ostream &out, BigNum &that)
 	{
-		cout.width(BASE_SIZE / 4);
-		cout.fill('0');
-		if (that.digits != NULL) for (int i = that.len; i--;) out << hex << int(that.digits[i]);
+		if (that.digits)
+		{
+			
+			for (int i = that.len; i--;)
+			{
+				cout.width(BASE_SIZE / 4);
+				cout.fill('0');
+				out << hex << (int)that.digits[i] << dec;
+			}
+		}
 		else out << ' ';
-		cout << dec;
 		return out;
 	}
-	
+*/
 
-
+	short cmp(const BigNum &that)
+	{
+		if (this == &that) return 0;
+		if (len < that.len) return -1;
+		if (len > that.len) return 1;
+		for (int i = len - 1; i >= 0; i--)
+		{
+			if (digits[i] < that.digits[i]) return -1;
+			if (digits[i] > that.digits[i]) return 1;
+		}
+		return 0;
+	}
 	bool operator== (const BigNum &that)
 	{
-		if (len != that.len) return false;
 		for (int i = 0; i < len; ++i) if (digits[i] != that.digits[i]) return false;
 		return true;
 	}
@@ -155,50 +190,48 @@ public:
 	}
 	bool operator< (const BigNum &that)
 	{
-		if (this == &that) return false;
-		if (len != that.len) return len < that.len;
-		for (int i = len; i--;) if (digits[i] != that.digits[i]) return digits[i] < that.digits[i];
-		return false;
+		if (cmp(that) < 0) return true;
+		else return false;
 	}
 	bool operator> (const BigNum &that)
 	{
-		if (this == &that) return false;
-		if (len != that.len) return len > that.len;
-		for (int i = len; i--;) if (digits[i] != that.digits[i]) return digits[i] > that.digits[i];
-		return false;
+		if (cmp(that) > 0) return true;
+		else return false;
 	}
 	bool operator<= (const BigNum &that)
 	{
-		return !(*this > that);
+		if (cmp(that) <= 0) return true;
+		else return false;
 	}
 	bool operator>= (const BigNum &that)
 	{
-		return !(*this < that);
+		if (cmp(that) >= 0) return true;
+		else return false;
 	}
 	BigNum operator+ (const BigNum &that)
 	{
-		BigNum sum(max(maxLen, that.maxLen) + 1);
+		BigNum sum(max(len, that.len) + 1);
 		int nm = min(len, that.len), j = 0, k = 0;
 		DBASE tmp = 0;
 		while (j < nm)
 		{
-			tmp = digits[j] + that.digits[j] + k;
-			sum.digits[j] = (BASE) tmp;
-			k = (BASE) (tmp >> BASE_SIZE);
+			tmp = DBASE(digits[j]) + DBASE(that.digits[j]) + DBASE(k);
+			sum.digits[j] = (BASE)tmp;
+			k = (BASE)(tmp >> BASE_SIZE);
 			++j;
 		}
 		while (j < len)
 		{
-			tmp = digits[j] + k;
-			sum.digits[j] = (BASE) tmp;
-			k = (BASE) (tmp >> BASE_SIZE);
+			tmp = DBASE(digits[j]) + DBASE(k);
+			sum.digits[j] = (BASE)tmp;
+			k = (BASE)(tmp >> BASE_SIZE);
 			++j;
 		}
 		while (j < that.len)
 		{
-			tmp = that.digits[j] + k;
-			sum.digits[j] = (BASE) tmp;
-			k = (BASE) (tmp >> BASE_SIZE);
+			tmp = DBASE(that.digits[j]) + DBASE(k);
+			sum.digits[j] = (BASE)tmp;
+			k = (BASE)(tmp >> BASE_SIZE);
 			++j;
 		}
 		sum.digits[j] = k;
@@ -212,17 +245,23 @@ public:
 	}
 	BigNum operator+ (BASE num)
 	{
-		int k = num, i = 0;
-		BigNum sum(maxLen + 1);
-		while (i < len)
+		DBASE tmp = 0;
+		BASE k = 0;
+		int newLen = maxLen;
+		if (len + 1 > maxLen) newLen = len + 1;
+		BigNum result(newLen);
+		tmp = DBASE(digits[0]) + DBASE(num) + DBASE(k);
+		k = (BASE)(tmp >> BASE_SIZE);
+		result.digits[0] = (BASE)tmp;
+		for (int i = 1; i < len; i++)
 		{
-			DBASE tmp = digits[i] + k;
-			k = (BASE) (tmp >> BASE_SIZE);
-			sum.digits[i] = tmp;
-			++i;
+			tmp = DBASE(digits[i]) + DBASE(k);
+			k = (BASE)(tmp >> BASE_SIZE);
+			result.digits[i] = (BASE)tmp;
 		}
-		sum.digits[i] = k;
-		return sum;
+		result.digits[len] = k;
+		result.lenNorm();
+		return result;
 	}
 	BigNum operator+= (BASE num)
 	{
@@ -231,25 +270,25 @@ public:
 	}
 	BigNum operator- (const BigNum &that)
 	{
-		BigNum sub(max(len, that.len), true);
+		BigNum sub(len);
 		if (*this >= that)
 		{
 			DBASE tmp;
 			int k = 0, j = 0;
 			while (j < that.len)
 			{
-				tmp = (1 << BASE_SIZE) + digits[j];
-				tmp -= (that.digits[j] + k);
-				sub.digits[j] = (BASE) tmp;
+				tmp = MAX_BASE_VALUE + digits[j];
+				tmp = tmp - that.digits[j] - k;
+				sub.digits[j] = (BASE)tmp;
 				k = !(tmp >> BASE_SIZE);
 				// ! is safer then ~
 				++j;
 			}
 			while (j < len)
 			{
-				tmp = (1 << BASE_SIZE) + digits[j];
+				tmp = MAX_BASE_VALUE + digits[j];
 				tmp -= k;
-				sub.digits[j] = (BASE) tmp;
+				sub.digits[j] = (BASE)tmp;
 				k = !(tmp >> BASE_SIZE);
 				++j;
 			}
@@ -258,7 +297,7 @@ public:
 		else
 		{
 			cerr << "right operand must be less or equal" << endl;
-			exit(-1);
+			return *this;
 		}
 		return sub;
 	}
@@ -269,14 +308,15 @@ public:
 	}
 	BigNum operator* (const BASE num)
 	{
-		BigNum mul(len + 1, true);
+		BigNum mul(len + 1);
+		mul.len = len + 1;
 		DBASE tmp = 0;
 		BASE k = 0;
 		for (int j = 0; j < len; ++j)
 		{
-			tmp = ((DBASE) digits[j]) * ((DBASE) num) + k;
+			tmp = (DBASE(digits[j]) * DBASE(num)) + DBASE(k);
+			mul.digits[j] = (BASE)tmp;
 			k = tmp >> BASE_SIZE;
-			mul.digits[j] = (BASE) tmp;
 		}
 		mul.digits[len] = k;
 		mul.lenNorm();
@@ -289,16 +329,16 @@ public:
 	}
 	BigNum operator* (const BigNum &that)
 	{
-		BigNum mul(len + that.len, true);
-		DBASE tmp, k;
+		BigNum mul(len + that.len);
+		DBASE tmp;
 		for (int i = 0; i < len; ++i)
 		{
-			k ^= k;
+			int k = 0;
 			for (int j = 0; j < that.len; ++j)
 			{
-				tmp = (DBASE) that.digits[j] * digits[i] + mul[i + j] + k;
-				mul.digits[i + j] = tmp;
-				k = tmp >> BASE_SIZE;
+				tmp = DBASE(that.digits[j]) * DBASE(digits[i]) + DBASE(mul.digits[i + j]) + DBASE(k);
+				mul.digits[i + j] = (BASE)tmp;
+				k = (BASE)(tmp >> BASE_SIZE);
 			}
 			mul.digits[that.len + i] += k;
 		}
@@ -312,17 +352,16 @@ public:
 	}
 	BigNum operator/ (const BASE num)
 	{
-		if (num <= 0) return 0;
-		BigNum d(len, true);
-		DBASE tmp = 0, k = 0;
-		for (int i = len - 1; i >= 0; --i)
+		DBASE tmp, k = 0;
+		BigNum q(len);
+		for(int i = len - 1; i >= 0; --i)
 		{
-			tmp = k << BASE_SIZE + digits[i];
+			tmp = (k << BASE_SIZE) + digits[i];
 			k = tmp % num;
-			d.digits[i] = tmp / num;
+			q.digits[i] = tmp / num;
 		}
-		d.lenNorm();
-		return d;
+		q.lenNorm();
+		return q;
 	}
 	BigNum operator/= (const BASE num)
 	{
@@ -331,14 +370,13 @@ public:
 	}
 	BASE operator% (const BASE num)
 	{
-		if (num <= 0) return 0;
-		DBASE tmp, mod;
+		DBASE tmp, r = 0;
 		for (int i = len - 1; i >= 0; --i)
 		{
-			tmp = mod << BASE_SIZE + digits[i];
-			mod = tmp % num;
+			tmp = ((DBASE) r << BASE_SIZE) + digits[i];
+			r = (BASE) (tmp % num);
 		}
-		return mod;
+		return r;
 	}
 	BASE operator%= (const BASE num)
 	{
@@ -348,59 +386,18 @@ public:
 	
 	BigNum operator/ (BigNum &that)
 	{
-		if (that.len == 1 && that.digits[0] != 0)
+		if (that.len == 1)
 		{
 			BigNum res = *this / that.digits[0];
 			return res;
 		}
-		if (that.len == 1 && that.digits[0] == 0 || *this == that) return 1;
-		if (*this < that) return 0;
-		BigNum q(len - that.len + 1);
-		DBASE d = MAX_BASE_VALUE / (that.digits[that.len - 1] + 1), q_, r_;
-		BigNum u(*this), v(that);	//чтобы было проще идти по алгоритму (соответствие переменных)
-		u *= d;
-		v *= d;
-		int j = len - that.len;
-		if (len == u.len)
+		if (that.digits[that.len - 1] == 0 || that.len < 1 || *this < that)
 		{
-			if (u.len == u.maxLen)
-			{
-				BigNum tmp(u);
-				delete[] u.digits;
-				u.digits = new BASE[u.maxLen + 1];
-				for (int i = 0; i < tmp.len; ++i) u.digits[i] = tmp.digits[i];
-				u.maxLen++;
-			}
-			u.digits[len] = 0;
-			u.len = len + 1;
+			cout << "that.digits[that.len - 1] == 0 || that.len < 1 || *this < that" << endl;
+			return *this;
 		}
-		while (j >= 0)
-		{
-			q_ = (u.digits[j + that.len] * MAX_BASE_VALUE + u.digits[j + that.len - 1]) / (v.digits[that.len - 1]);
-			r_ = (u.digits[j + that.len] * MAX_BASE_VALUE + u.digits[j + that.len - 1]) % (v.digits[that.len - 1]);
-			if (q_ >= MAX_BASE_VALUE || q_ * v.digits[that.len - 2] > MAX_BASE_VALUE * r_ + u.digits[j + that.len - 2])
-			{
-				--q_;
-				r_ += v.digits[that.len - 1];
-				if ((r_ < MAX_BASE_VALUE) &&(q_ >= MAX_BASE_VALUE || q_ * v.digits[that.len - 2] > MAX_BASE_VALUE * r_ + u.digits[j + that.len - 2])) 
-				{
-					--q_;
-					r_ += v.digits[that.len - 1];
-				}
-			}
-			q.digits[j] = q_;
-			int k = u.weirdSub(v * q_, j);
-			if (k)
-			{
-				q.digits[j]--;
-				u.weirdAdd(v, j);
-			}
-			--j;
-		}
-		q.lenNorm();
-		return q;
+		return divmod(*this, that).first;
 	}
-
 	BigNum operator/= (BigNum &that)
 	{
 		*this = *this / that;
@@ -409,112 +406,106 @@ public:
 	
 	BigNum operator% (BigNum &that)
 	{
-		if (that.len == 1 && that.digits[0] != 0)
+		if (that.len == 1)
 		{
-			BigNum r(1, 1);
+			BigNum r(1);
 			r.digits[0] = *this % that.digits[0];
 			return r;
 		}
-		if (that.digits[0] == 0) throw invalid_argument("ERROR: DIVISION BY ZERO");
-		if (that == *this)
+		if (that.digits[that.len - 1] == 0 || that.len < 1 || *this < that)
 		{
-			BigNum r = 0;
-			return r;
+			cout << "that.digits[that.len - 1] == 0 || that.len < 1 || *this < that" << endl;
+			return *this;
 		}
-		if(*this < that) return *this;
-		BigNum r(that.len - 1), u(*this), v(that);
-		DBASE d = MAX_BASE_VALUE / (that.digits[that.len - 1] + 1);
-		u *= d;
-		v *= d;
-		if (len == u.len)
-		{
-			if (u.len == u.maxLen)
-			{
-				BigNum tmp(u);
-				delete[] u.digits;
-				u.digits = new BASE[u.maxLen + 1];
-				for (int i = 0; i < tmp.len; ++i) u.digits[i] = tmp.digits[i];
-				u.maxLen++;
-			}
-			u.digits[len] = 0;
-			u.len = len + 1;
-		}
-		int j = len - that.len;
-		DBASE q_, r_;
-		do
-		{
-			q_ = ((u.digits[j + that.len] * MAX_BASE_VALUE+u.digits[j + that.len - 1]) / v.digits[that.len - 1]);
-			r_ = ((u.digits[j + that.len] * MAX_BASE_VALUE+u.digits[j + that.len - 1]) % v.digits[that.len - 1]);
-			if (q_ >= MAX_BASE_VALUE || q_ * v.digits[that.len - 2] > MAX_BASE_VALUE * r_ + u.digits[j + that.len - 2])
-			{
-				--q_;
-				r_ += v.digits[that.len - 1];
-				if ((r_ < MAX_BASE_VALUE) &&(q_ >= MAX_BASE_VALUE || q_ * v.digits[that.len - 2] > MAX_BASE_VALUE * r_ + u.digits[j + that.len - 2])) 
-				{
-					--q_;
-					//r_ += v.digits[that.len - 1];
-				}
-			}
-			int k = u.weirdSub(v * q_, j);
-			if (k) u.weirdAdd(v, j);
-			--j;
-		} while (j >= 0);
-		r = u / d;
-		r.lenNorm();
-		return r;
+		return divmod(*this, that).second;
 	}
 	BigNum operator%= (BigNum &that)
 	{
 		*this = *this % that;
 		return *this;
 	}
-	
 
-/*
-
-	friend istream & operator>> (istream &in, BigNum &that)
+	friend pair<BigNum, BigNum> divmod(BigNum &a, BigNum &that)	//a div that && a mod that
 	{
-		string s;
-		in >> s;
-		if (s.length() > that.maxLen)
+		BigNum divident = a,
+		divisor = that,
+		q(a.len);
+		q.len = 1;
+		//нормализация (D1)
+		DBASE q_ = 0, r_ = 0, b = MAX_BASE_VALUE,
+		d = MAX_BASE_VALUE / ((DBASE)that.digits[that.len - 1] + (DBASE)1);;
+		if(d != 1)
 		{
-			cerr << "s.length() > that.maxLen" << endl;
-			exit(0);
+			divident *= d;
+			divisor *= d;
 		}
-		DBASE tmp = 0;
-		BASE k = 0;
-		delete[] that.digits;
-		that.len = s.length();
-		that.digits = new BASE[that.maxLen];
-		for (int i = 0; i < that.maxLen; ++i) that.digits[i] = 0;
-		for (int i = 0; i < that.len; ++i)
+		BigNum tmpDivident = divident;
+		if (a.len == divident.len)
 		{
-			if (s[i] >= '0' && s[i] <= '9')
+			delete[] divident.digits;
+			divident.digits = new BASE[tmpDivident.len + 1];
+			divident.maxLen++;
+			divident.len++;
+			for(int i = 0; i < tmpDivident.len; ++i) divident.digits[i] = tmpDivident.digits[i];
+			divident.digits[divident.len - 1] = 0;
+		}
+		//начальная установка индекса (D2)
+		int j = a.len - that.len, k = divisor.len;
+		for(; j >= 0; --j)	//шаг цикла (D7)
+		{
+			//вычисление q^ и r^, проверка сложного условия (D3)
+			q_ = (((DBASE(divident.digits[j + k]) << BASE_SIZE)) + divident.digits[j + k - 1]) / divisor.digits[k - 1];
+			r_ = (((DBASE(divident.digits[j + k]) << BASE_SIZE)) + divident.digits[j + k - 1]) % divisor.digits[k - 1];
+			if ((q_ == b) || ((q_ * divisor.digits[k - 2]) > (b * r_ + divident.digits[j + k - 2])))
 			{
-				char _ch = s[i] - '0';
-				that *= 10;
-				that +=_ch;
+				--q_;
+				r_ = r_ + divisor.digits[k - 1];
+				if ((r_ < b) && ((q_ == b) || (q_ * divisor.digits[k - 2] > b * r_ + divident.digits[j + k - 2]))) --q_;
 			}
-			else break;
+			int dl = divisor.len;
+			BigNum tmpDivisor = divisor * q_;
+			//умножить-вычесть (D4)
+			DBASE tmp = 0, k__ = 0;
+			int ix = j, jx = dl, i = ix, l = 0;
+			while (i <= ix + jx)
+			{
+				tmp = MAX_BASE_VALUE + DBASE(divident.digits[i]);
+				tmp -= DBASE(tmpDivisor.digits[l]) + DBASE(k__);
+				divident.digits[i] = (BASE) tmp;
+				k__ = !(tmp >> BASE_SIZE);
+				++i;
+				++l;
+			}
+			divident.lenNorm();
+			DBASE fl = k__;
+			//присваивание и проверка условия (D5)
+			q.digits[j] = q_;
+			q.len++;
+			if (fl < 0)	
+			{
+				//компенсировать сложение (D6)
+				i = j;
+				l = 0;
+				int ln = i + divisor.len;
+				for (; i <= ln; ++i)
+				{
+					DBASE tmp = DBASE(divident.digits[i]) + DBASE(divisor.digits[l]) + DBASE(k__);
+					k__ = (BASE) (tmp >> BASE_SIZE);
+					divident.digits[i] = (BASE) tmp;
+					++l;
+				}
+				divident.digits[i] = k__;
+				divident.lenNorm();
+				q.digits[j]--;
+			}
 		}
-		that.lenNorm();
-		return in;
+		divident.len = divisor.len;
+		divident.maxLen = divisor.maxLen;
+		BigNum r = divident / d;	//D8 - денормализация для модуля
+		q.lenNorm();
+		return make_pair(q, r);
 	}
-	friend ostream & operator<< (ostream &out, BigNum &that)
-	{
-		string s = "";
-		BigNum tmp = that;
-		while (tmp.digits[0] != 0 || tmp.len > 1)
-		{
-			s.push_back(tmp % 10 + '0');
-			tmp /= 10;
-		}
-		out << s;
-		return out;
-	}
-
-*/
-
+	
 	void lenNorm()
 	{
 		len = maxLen;
@@ -525,25 +516,66 @@ public:
 int main()
 {
 	srand(time(0));
-	BigNum a(4, false), b(4, false), c, d(4, false), cop(a);
-	cout << "a = " << a << endl << "b = " << b << endl << "d = " << d << endl << "copy of a = " << cop << endl;
-	c = a + b;
-	cout << "a + b = " << c << endl;
-	c -= a;
-	cout << "a + b - a = " << c << endl;
-	c = a * b;
-	cout << "a * b = " << c << endl;
-	c *= d;
-	cout << "(a * b) * d = " << c << endl;
-	cout << "a = " << a << " b = " << b << endl;
-	c = a / b;
-	cout << "a / b = " << c << endl;
-	c = b / a;
-	cout << "b / a = " << c << endl;
+	BigNum a(2, false), b(2, false), c, d;
+	cout << a << " <-a b-> " << b << endl;
+	d = a / b;
 	c = a % b;
-	cout << "a % b = " << c << endl;
-	c = b % a;
-	cout << "b % a = " << c << endl;
+	cout << "a / b = " << d << endl
+	<< "a % b = " << c << endl;
+	if ((b * d + c) == a) cout << "division and module are correct" << endl;
+	else
+	{
+		cout << "divisor must not exceed dividend" << endl;
+		BigNum tmpBN_ = a;
+		a = b;
+		b = tmpBN_;
+		d = a / b;
+		c = a % b;
+		cout << "a / b = " << d << endl
+		<< "a % b = " << c << endl;
+		if ((b * d + c) == a) cout << "division and module are correct" << endl;
+	}
+	
+	cout << "TEST" << endl;
+	int M = 1000, T = 1000, tmp=0;
+	BigNum A, B, C, D, tmpBN;
+	do
+	{
+		int n = rand() % M + 1,
+		m = rand() % n + 1;
+		A = BigNum(n, false);
+		B = BigNum(m, false);
+		if (A < B)
+		{
+			tmpBN = A;
+			A = B;
+			B = tmpBN;
+		}
+		C = A / B;
+		D = A % B;
+		BigNum asdf = C * B, qwerty = C * B + D;	//4isto 4toby vyvodit'
+		if (A != C * B + D)
+		{
+			cout << "A = " << A << endl << "B = " << B << endl
+			 << "C = " << C << endl
+			 << "D = " << D << endl
+			 << "C * B = " << asdf << endl
+			 << "C * B + D = " << qwerty << endl
+			 << "1 condition" << endl;
+		}		
+		if (A - D != C * B) cout << "2 condition" << endl;
+		if (D >= B) cout << "3 condition" << endl;
+		cout << "T = " << T << " n = " << n << " m = " << m << endl;
+		--T;
+	}
+	while (A == C * B + D && A-D == C * B && D < B && T > 0);
+	//if (T > 0)
+		//cout << "failed at T=" << T + 1 << endl
+		//<< "A = " << A << endl
+		//<< "B = " << B << endl
+		//<< "C = " << C << endl
+		//<< "D = " << D << endl;
 	//calculatori.ru
+	
 	return 1;
 }
